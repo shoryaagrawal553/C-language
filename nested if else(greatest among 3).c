107eae480235d43ae3041f25c9d995fc57ab41fa
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-01-25 18:18:22 +0530

    Add files via upload

---
+++ nested if else(greatest among 3).c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("Enter three numbers: ");
+    scanf("%d%d%d", &a, &b, &c);
+    if(a > b);
+    {
+        if(a > c)
+        {
+            printf("A is the greatest no.");
+        }
+        else
+        {
+            if(c > a)
+            {
+              printf("C is the greatest no.");  
+            }
+            else 
+            {
+                if(c > b)
+        {
+            printf("C is the greatest no.");
+        }
+        else
+        {
+            printf("B is the greatest no.");
+        }
+            }
+        }
+    }
+}
+        
+    
+
+        
+        
+    
+    
---
+++ nested if else.c
@@ -0,0 +1,26 @@
+//greatest among three numbers
+# include<stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("Enter three no. ");
+    scanf("%d%d%d", &a, &b, &c);
+    if(a > b)
+    {
+        if(a > c)
+        {
+            printf("a is the greatest no.");
+        }
+    }
+    else
+    {
+        if("b > c");
+        {
+            printf("b is the greatest no.");
+        }
+        else
+        {
+            printf("c is the greatst no.");
+        }
+    }
+}
