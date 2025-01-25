2dd778e1c8e1ee7b35f399d59786eda0db879a95
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-01-25 18:07:07 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,60 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int H, E, M, S, total;
-    float per;
-    printf("Enter marks of hindi");
-    scanf("%d", &H);
-    printf("Enter marks of Enlish");
-    scanf("%d", &E);
-    printf("Enter marks of Maths");
-    scanf("%d", &M);
-    printf("Enter marks of science");
-    scanf("%d", &S);
-    total = H + E + M + S;
-    printf("\ntotal marks out of 400 is %d", total);
-    per = (total * 100) / 400;
-    printf("\n Percentage is %.2f", per);
-    if(per > 75)
+    int year;
+    printf("Enter the Year ");
+    scanf("%d", &year);
+    if(year%4==0)
     {
-        printf("\nPassed with I div honours");
+        printf("\nIt is a leap year");
     }
     else
     {
-        if(per > 60 && per <= 75 )
-        {
-            printf("\nPassed with I div");
-        }
-        else
-        {
-            if(per > 45 && per <= 60 )
-            {
-             printf("\nPassed with II div");   
-            }
-            else
-            {
-                if(per > 33 && per <= 45 )
-                {
-                 printf("\nPassed with III div");   
-                }
-                else
-                {
-                    if(per <= 33)
-                    {
-                    printf("\nFailed");
-                    }
-                }
-            }
-        }
+        printf("\nIt is not a leap year");
     }
 }
-    
-    
-    
-    
-    
-    
-    
-    
-
-
