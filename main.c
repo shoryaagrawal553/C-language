a2f2f1bf538599a7786274f69c851b8cc2f5c7d0
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-01-26 13:42:17 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    //for (looping)
+    int n,i;
+    printf("Enter how many times you want it: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+    printf("\nHello World");
+    }
+}
