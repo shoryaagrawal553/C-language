25dbc2c03dac194cad399a1918118af242081a1c
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-06 18:31:31 +0530

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
+
 int main()
 {
+    // Declare matrix A and variables for iteration and dimensions
     int A[100][100], i, j, r, c;
+    
+    // Ask the user for the number of rows and columns
     printf("Enter how many rows and columns you want ");
     scanf("%d%d", &r, &c);
+    
+    // Get the elements of the matrix from the user
     for(i=0; i<r; i++)
     {
         for(j=0; j<c; j++)
@@ -12,6 +18,8 @@ int main()
             scanf("%d", &A[i][j]);
         }
     }
+    
+    // Print the matrix
     printf("\nThe matrix is\n");
     for(i=0; i<r; i++)
     {
