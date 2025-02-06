e86aab20dc2b314e9e5f025a3fbc8e9700748858
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-06 18:30:21 +0530

    Update matrix multiplicatiion.c

--- matrix multiplicatiion.c
+++ matrix multiplicatiion.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
-int
-main ()
+
+int main ()
 {
+  // Declare matrices and variables
   int a[100][100], b[100][100], i, j, k, r, c, mul[100][100];
+  
+  // Ask the user for the number of rows and columns for the first matrix
   printf ("Enter how many rows and columns you want for first matrix A ");
   scanf ("%d%d", &r, &c);
+  
+  // Get the elements of the first matrix from the user
   for (i = 0; i < r; i++)
     {
       for (j = 0; j < c; j++)
@@ -13,6 +18,8 @@ main ()
 	  scanf ("%d", &a[i][j]);
 	}
     }
+    
+  // Print the first matrix
   printf ("\nThe matrix A is \n");
   for (i = 0; i < r; i++)
     {
@@ -22,8 +29,12 @@ main ()
 	}
       printf ("\n");
     }
+    
+  // Ask the user for the number of rows and columns for the second matrix
   printf ("Enter how many rows and columns you want for second matrix B ");
   scanf ("%d%d", &r, &c);
+  
+  // Get the elements of the second matrix from the user
   for (i = 0; i < r; i++)
     {
       for (j = 0; j < c; j++)
@@ -32,6 +43,8 @@ main ()
 	  scanf ("%d", &b[i][j]);
 	}
     }
+    
+  // Print the second matrix
   printf ("\nThe matrix B is \n");
   for (i = 0; i < r; i++)
     {
@@ -41,6 +54,8 @@ main ()
 	}
       printf ("\n");
     }
+    
+  // Multiply the matrices
   for (i = 0; i < r; i++)
     {
       for (j = 0; j < c; j++)
@@ -52,6 +67,8 @@ main ()
 	    }
 	}
     }
+    
+  // Print the result of the matrix multiplication
   printf ("\nThe matrix after multiplication of matrices a and b is \n");
   for (i = 0; i < r; i++)
     {
