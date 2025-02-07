22aff3435244608127164829e3e0e6b3bea2fa6d
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-07 15:47:54 +0530

    Update pointer.c

--- pointer.c
+++ pointer.c
@@ -1,10 +1,17 @@
-#include<stdio.h>
-int main()
+#include<stdio.h>  // Include standard input-output header file
+
+int main() // Main function
 {
-    int a, *b;
-    a = 10;
-    b = &a;
+    int a, *b; // Declare an integer variable 'a' and an integer pointer 'b'
+    a = 10; // Initialize 'a' with value 10
+    b = &a; // Assign the address of 'a' to pointer 'b'
+    
+    // Print the value of 'a'
     printf("Value of a is %d", a);
+    
+    // Print the address of 'a'
     printf("\nAddress of a is %x", &a);
+    
+    // Print the value of pointer 'b' (which is the address of 'a')
     printf("\nValue of b is %x", b);
 }
