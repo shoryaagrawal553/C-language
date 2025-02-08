6b14590e557e022ac27c31b91504d82fe89466c9
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-08 20:21:20 +0530

    Update tringular pyramid.c

--- tringular pyramid.c
+++ tringular pyramid.c
@@ -1,23 +1,30 @@
 #include<stdio.h>
+
 int main()
 {
-    int n, i, j, k;
-    printf("enter any number ");
-    scanf("%d", &n);
+    int n, i, j, k;  // Declare variables for the number of rows and loop counters
+    printf("enter any number ");  // Prompt user for input
+    scanf("%d", &n);  // Read the number of rows from the user
+    
+    // Loop to print each row of the pyramid
     for(i=1; i<=n; i++)
     {
+        // Print spaces to create the pyramid shape
         for(j=n-i; j>=0; j--)
         {
             printf(" ");
         }
+        // Print asterisks for the current row
         for(k=1; k<=i; k++)
         {
             printf(" *");
         }
-        printf("\n");
+        printf("\n");  // Move to the next line after printing each row
     }
-    //      OR (self)
-   /* for(j=1; j<=n-i; j++)
+
+    // Alternative approach commented out
+    /* 
+    for(j=1; j<=n-i; j++)
     {
         printf(" ");
     }
@@ -26,30 +33,5 @@ int main()
         printf(" *");
     }
     printf("\n");
-    }*/
+    */
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
