c15f3b2cfca5f1e19ec9994e668e703723c53c03
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-08 20:40:07 +0530

    Update sum of n no. using array.c

--- sum of n no. using array.c
+++ sum of n no. using array.c
@@ -1,17 +1,26 @@
 #include<stdio.h>
+
 int main() 
 {
-    int n, i, a[1000], sum=0;
-    printf("Enter how many numbers you want to add ");
-    scanf("%d", &n);
-    for(i=0; i<n; i++)
+    int n, i, a[1000], sum = 0;  // Declare variables: n for number of inputs, i for loop counter, a for storing numbers, and sum for storing the sum of numbers
+
+    printf("Enter how many numbers you want to add ");  // Prompt the user to enter the number of inputs
+    scanf("%d", &n);  // Read the number of inputs from the user
+
+    // Loop to read n numbers into the array a
+    for(i = 0; i < n; i++)
     {
-        printf("Enter single a digit no. " );
-        scanf("%d", &a[i]);
+        printf("Enter a single digit no. ");  // Prompt the user to enter a number
+        scanf("%d", &a[i]);  // Read the number and store it in the array a
     }
-    for(i=0; i<n; i++)
+
+    // Loop to calculate the sum of all numbers in the array
+    for(i = 0; i < n; i++)
     {
-        sum+=a[i];    // sum = sum + a[i]    now sum=0   sum = 0 + 5   eg: a[0] = 5
+        sum += a[i];  // Add the current number to the sum
+        // sum = sum + a[i]    // Example: if sum = 0 and a[0] = 5, then sum = 0 + 5
     }
+
+    // Print the sum of the numbers
     printf("\nSum is %d", sum);
 }
